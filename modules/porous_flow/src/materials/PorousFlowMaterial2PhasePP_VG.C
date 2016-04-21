52f3cc9e5a85d7412eedd6a6eacd14cc73618c55
Chris Green <chris.green@csiro.au>
2016-04-21 10:19:56 +1000

    Remove unnecessary temperature variable - already in base class

--- modules/porous_flow/src/materials/PorousFlowMaterial2PhasePP_VG.C
+++ modules/porous_flow/src/materials/PorousFlowMaterial2PhasePP_VG.C
@@ -15,7 +15,6 @@ InputParameters validParams<PorousFlowMaterial2PhasePP_VG>()
 
   params.addRequiredCoupledVar("phase0_porepressure", "Variable that is the porepressure of phase 0 (eg, the water phase).  It will be <= phase1_porepressure.");
   params.addRequiredCoupledVar("phase1_porepressure", "Variable that is the porepressure of phase 1 (eg, the gas phase)");
-  params.addCoupledVar("phase0_temperature", 20.0, "Fluid temperature of phase 0");
   params.addRequiredRangeCheckedParam<Real>("al", "al > 0", "van-Genuchten alpha parameter.  Must be positive.  effectiveSaturation = (1 + (-al*P)^(1/(1-m)))^(-m), where P = phase0_porepressure - phase1_porepressure <= 0");
   params.addRequiredRangeCheckedParam<Real>("m", "m > 0 & m < 1", "van-Genuchten m parameter.  Must be between 0 and 1, and optimally should be set to >0.5   EffectiveSaturation = (1 + (-al*p)^(1/(1-m)))^(-m)");
   params.addClassDescription("This Material calculates the 2 porepressures and the 2 saturations in a 2-phase isothermal situation, and derivatives of these with respect to the PorousFlowVariables.  A van-Genuchten capillary suction function is assumed");
@@ -36,11 +35,7 @@ PorousFlowMaterial2PhasePP_VG::PorousFlowMaterial2PhasePP_VG(const InputParamete
     _phase1_porepressure_nodal(coupledNodalValue("phase1_porepressure")),
     _phase1_porepressure_qp(coupledValue("phase1_porepressure")),
     _phase1_gradp_qp(coupledGradient("phase1_porepressure")),
-    _phase1_porepressure_varnum(coupled("phase1_porepressure")),
-
-    _phase0_temperature_nodal(coupledNodalValue("phase0_temperature")),
-    _phase0_temperature_qp(coupledValue("phase0_temperature")),
-    _phase0_temperature_varnum(coupled("phase0_temperature"))
+    _phase1_porepressure_varnum(coupled("phase1_porepressure"))
 {
   if (_dictator_UO.num_phases() != 2)
     mooseError("The Dictator announces that the number of phases is " << _dictator_UO.num_phases() << " whereas PorousFlowMaterial2PhasePP_VG can only be used for 2-phase simulation.  When you have an efficient government, you have a dictatorship.");
@@ -88,14 +83,14 @@ PorousFlowMaterial2PhasePP_VG::initQpStatefulProperties()
     _dtemperature_qp_dvar[_qp][phase].assign(_dictator_UO.num_v(), 0.0);
   }
 
-  // _temperature is only dependent on _phase0_temperature, and its derivative is = 1
-  if (!(_dictator_UO.not_porflow_var(_phase0_temperature_varnum)))
+  // _temperature is only dependent on _temperature, and its derivative is = 1
+  if (!(_dictator_UO.not_porflow_var(_temperature_varnum)))
   {
     // _phase0_temperature is a porflow variable
-    _dtemperature_nodal_dvar[_qp][0][_dictator_UO.porflow_var_num(_phase0_temperature_varnum)] = 1.0;
-    _dtemperature_nodal_dvar[_qp][1][_dictator_UO.porflow_var_num(_phase0_temperature_varnum)] = 1.0;
-    _dtemperature_qp_dvar[_qp][0][_dictator_UO.porflow_var_num(_phase0_temperature_varnum)] = 1.0;
-    _dtemperature_qp_dvar[_qp][1][_dictator_UO.porflow_var_num(_phase0_temperature_varnum)] = 1.0;
+    _dtemperature_nodal_dvar[_qp][0][_dictator_UO.porflow_var_num(_temperature_varnum)] = 1.0;
+    _dtemperature_nodal_dvar[_qp][1][_dictator_UO.porflow_var_num(_temperature_varnum)] = 1.0;
+    _dtemperature_qp_dvar[_qp][0][_dictator_UO.porflow_var_num(_temperature_varnum)] = 1.0;
+    _dtemperature_qp_dvar[_qp][1][_dictator_UO.porflow_var_num(_temperature_varnum)] = 1.0;
   }
 }
 
@@ -177,8 +172,8 @@ PorousFlowMaterial2PhasePP_VG::buildQpPPSS()
   _grads_qp[_qp][1] = -_grads_qp[_qp][0];
 
   /// Temperature is the same in each phase presently
-  _temperature_nodal[_qp][0] = _phase0_temperature_nodal[_qp];
-  _temperature_nodal[_qp][1] = _phase0_temperature_nodal[_qp];
-  _temperature_qp[_qp][0] = _phase0_temperature_qp[_qp];
-  _temperature_qp[_qp][1] = _phase0_temperature_qp[_qp];
+  _temperature_nodal[_qp][0] = _temperature_nodal_var[_qp];
+  _temperature_nodal[_qp][1] = _temperature_nodal_var[_qp];
+  _temperature_qp[_qp][0] = _temperature_qp_var[_qp];
+  _temperature_qp[_qp][1] = _temperature_qp_var[_qp];
 }
