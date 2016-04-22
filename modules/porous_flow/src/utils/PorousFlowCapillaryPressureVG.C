b18d238ef992c037ef43340aee41d0deb7ab3340
Chris Green <chris.green@csiro.au>
2016-04-22 17:16:43 +1000

    Also guard against inf in capillaryPressure

--- modules/porous_flow/src/utils/PorousFlowCapillaryPressureVG.C
+++ modules/porous_flow/src/utils/PorousFlowCapillaryPressureVG.C
@@ -17,11 +17,13 @@ Real
 PorousFlowCapillaryPressureVG::capillaryPressure(Real s, Real m, Real sat_r, Real sat_s, Real p0, Real pc_max)
 {
   Real sat_eff = PorousFlowCapillaryPressureVG::effectiveSaturation(s, sat_r, sat_s);
-  Real pc;
+  Real pc = 0.0;
   if (0.0 < sat_eff < 1.0)
-   pc = p0 * std::pow(std::pow(sat_eff, - 1.0 / m) - 1.0, 1.0 - m);
-  else
-   pc = 0.0;
+  {
+    Real a = std::pow(sat_eff, - 1.0 / m) - 1.0;
+    if (a > 0.0)
+      pc = p0 * std::pow(a, 1.0 - m);
+  }
 
   /// Return the max of this and pc_max
   return std::max(pc, pc_max);
@@ -31,18 +33,14 @@ Real
 PorousFlowCapillaryPressureVG::dCapillaryPressure(Real s, Real m, Real sat_r, Real sat_s, Real p0)
 {
   Real sat_eff = PorousFlowCapillaryPressureVG::effectiveSaturation(s, sat_r, sat_s);
-  Real dpc;
+  Real dpc = 0.0;
 
   if (0.0 < sat_eff < 1.0)
   {
     Real a = std::pow(sat_eff, - 1.0 / m) - 1.0;
     if (a > 0.0)
       dpc = p0 * (1.0 - m) * std::pow(a, - m) * std::pow(sat_eff, - 1.0 - 1.0 / m) / (m * (sat_s - sat_r));
-    else
-      dpc = 0.0;
   }
-  else
-    dpc = 0.0;
 
   return dpc;
 }
@@ -50,24 +48,19 @@ PorousFlowCapillaryPressureVG::dCapillaryPressure(Real s, Real m, Real sat_r, Re
 Real
 PorousFlowCapillaryPressureVG::d2CapillaryPressure(Real s, Real m, Real sat_r, Real sat_s, Real p0)
 {
-  Real d2cp;
+  Real d2pc = 0.0;
   Real sat_eff = PorousFlowCapillaryPressureVG::effectiveSaturation(s, sat_r, sat_s);
 
   if (0.0 < sat_eff < 1.0)
   {
     Real a = std::pow(sat_eff, - 1.0 / m) - 1.0;
-    std::cout << "a " << a << std::endl;
     if (a > 0.0)
     {
-      d2cp = std::pow(a, - 1.0 - m) * std::pow(sat_eff, - 2.0 - 2.0 / m) - ((1.0 + m) / m) * std::pow(a, - m) *
+      d2pc = std::pow(a, - 1.0 - m) * std::pow(sat_eff, - 2.0 - 2.0 / m) - ((1.0 + m) / m) * std::pow(a, - m) *
         std::pow(sat_eff, - 1.0 / m - 2.0);
-      d2cp *= p0 * (1.0 - m) / m / (sat_s - sat_r) / (sat_s - sat_r);
+      d2pc *= p0 * (1.0 - m) / m / (sat_s - sat_r) / (sat_s - sat_r);
     }
-    else
-      d2cp = 0.0;
   }
-  else
-    d2cp = 0.0;
 
-  return d2cp;
+  return d2pc;
 }
