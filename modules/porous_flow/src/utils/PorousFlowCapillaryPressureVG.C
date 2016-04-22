9dad521ff7b6c5077b943b964400e343675204aa
Chris Green <chris.green@csiro.au>
2016-04-22 15:09:41 +1000

    Check VG capillary pressure so that no infs or nans can be accidently produced

--- modules/porous_flow/src/utils/PorousFlowCapillaryPressureVG.C
+++ modules/porous_flow/src/utils/PorousFlowCapillaryPressureVG.C
@@ -17,7 +17,11 @@ Real
 PorousFlowCapillaryPressureVG::capillaryPressure(Real s, Real m, Real sat_r, Real sat_s, Real p0, Real pc_max)
 {
   Real sat_eff = PorousFlowCapillaryPressureVG::effectiveSaturation(s, sat_r, sat_s);
-  Real pc = p0 * std::pow(std::pow(sat_eff, - 1.0 / m) - 1.0, 1.0 - m);
+  Real pc;
+  if (0.0 < sat_eff < 1.0)
+   pc = p0 * std::pow(std::pow(sat_eff, - 1.0 / m) - 1.0, 1.0 - m);
+  else
+   pc = 0.0;
 
   /// Return the max of this and pc_max
   return std::max(pc, pc_max);
@@ -27,21 +31,43 @@ Real
 PorousFlowCapillaryPressureVG::dCapillaryPressure(Real s, Real m, Real sat_r, Real sat_s, Real p0)
 {
   Real sat_eff = PorousFlowCapillaryPressureVG::effectiveSaturation(s, sat_r, sat_s);
+  Real dpc;
 
-  return p0 * (1.0 - m) * std::pow(std::pow(sat_eff, - 1.0 / m) - 1.0, - m) * std::pow(sat_eff, -1.0 - 1.0 / m) / (m * (sat_s - sat_r));
+  if (0.0 < sat_eff < 1.0)
+  {
+    Real a = std::pow(sat_eff, - 1.0 / m) - 1.0;
+    if (a > 0.0)
+      dpc = p0 * (1.0 - m) * std::pow(a, - m) * std::pow(sat_eff, - 1.0 - 1.0 / m) / (m * (sat_s - sat_r));
+    else
+      dpc = 0.0;
+  }
+  else
+    dpc = 0.0;
+
+  return dpc;
 }
 
 Real
 PorousFlowCapillaryPressureVG::d2CapillaryPressure(Real s, Real m, Real sat_r, Real sat_s, Real p0)
 {
   Real d2cp;
-  Real a;
   Real sat_eff = PorousFlowCapillaryPressureVG::effectiveSaturation(s, sat_r, sat_s);
 
-  a = std::pow(sat_eff, - 1.0 / m) - 1.0;
-  d2cp = std::pow(a, - 1.0 - m) * std::pow(sat_eff, - 2.0 - 2.0 / m) - ((1.0 + m) / m) * std::pow(a, - m) *
-    std::pow(sat_eff, - 1.0 / m - 2.0);
-  d2cp *= p0 * (1.0 - m) / m / (sat_s - sat_r) / (sat_s - sat_r);
+  if (0.0 < sat_eff < 1.0)
+  {
+    Real a = std::pow(sat_eff, - 1.0 / m) - 1.0;
+    std::cout << "a " << a << std::endl;
+    if (a > 0.0)
+    {
+      d2cp = std::pow(a, - 1.0 - m) * std::pow(sat_eff, - 2.0 - 2.0 / m) - ((1.0 + m) / m) * std::pow(a, - m) *
+        std::pow(sat_eff, - 1.0 / m - 2.0);
+      d2cp *= p0 * (1.0 - m) / m / (sat_s - sat_r) / (sat_s - sat_r);
+    }
+    else
+      d2cp = 0.0;
+  }
+  else
+    d2cp = 0.0;
 
   return d2cp;
 }
